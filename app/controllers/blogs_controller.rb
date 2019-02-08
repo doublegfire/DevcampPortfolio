24a1fe010316b4aa86c5b719a5d19c2e298dd54c
doubleg <darnel_free@yahoo.com>
2019-02-08 17:48:36 -0600

    Added new and edit buttons for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -42,11 +42,8 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
-      else
-        format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' } else   
+           format.html { render :edit }  
       end
     end
   end
